5956d04edda26e409f5ddb4fbd2fbfef63461f37
mohamad akbarizade <akbarizadehmohammad.75@gmail.com>
2021-04-07 18:22:55 +0430

    initial code that works

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import glob
+import serial
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+# init serial port and bound
+# bound rate on two ports must be the same
+ser = serial.Serial('COM4', 9600)
+print(ser.portstr)
+
+# send data via serial port
+serialcmd = ("HELP?;")
+ser.write(serialcmd.encode())
+ser.close()
+
+print(serial_ports())
